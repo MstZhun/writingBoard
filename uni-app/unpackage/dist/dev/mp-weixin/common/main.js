e6f3e18cbb28207a3b070bac3857918a78d6d985
廖启准 <1141446749@qq.com>
2020-09-04 14:48:42 +0800

    wx,手写板

---
+++ uni-app/unpackage/dist/dev/mp-weixin/common/main.js
@@ -0,0 +1,13 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["common/main"],[
+/* 0 */
+/*!********************************************************************!*\
+  !*** /Users/duanpeipei/Documents/byb-project/writing_demo/main.js ***!
+  \********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/main.js):\nError: ENOENT: no such file or directory, open '/Users/duanpeipei/Documents/byb-project/writing_demo/main.js'");
+
+/***/ })
+],[[0,"common/runtime"]]]);
+//# sourceMappingURL=../../.sourcemap/mp-weixin/common/main.js.map
---
+++ uni-app/unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,15 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/index/index"],{
+
+/***/ 11:
+/*!*****************************************************************************************************!*\
+  !*** /Users/duanpeipei/Documents/byb-project/writing_demo/main.js?{"page":"pages%2Findex%2Findex"} ***!
+  \*****************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/main.js):\nError: ENOENT: no such file or directory, open '/Users/duanpeipei/Documents/byb-project/writing_demo/main.js'");
+
+/***/ })
+
+},[[11,"common/runtime"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/index/index.js.map
